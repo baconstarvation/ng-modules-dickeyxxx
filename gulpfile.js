6de3b0495aa54aba7a5fb309f8912c47c660d0b6
baconstarvation <ant.ucd@gmail.com>
2014-08-16 19:43:32 -0700

    package json echo'ed. gulpfile created. time to gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,8 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
-var sourcemaps = require('gulp-sourcemaps')
-var uglify = require('gulp-uglify')
-var ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('js', function () {
-  gulp.src(['src/**/module.js', 'src/**/*.js'])
-    .pipe(sourcemaps.init())
-      .pipe(concat('app.js'))
-      .pipe(ngAnnotate())
-      .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('.'))
-})
-
-gulp.task('watch', ['js'], function () {
-  gulp.watch('src/**/*.js', ['js'])
+	gulp.src(['src/**/module.js', 'src/**/*.js'])
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('.'))
 })
---
+++ how-not-to-do-it/using_browserify.js
@@ -0,0 +1,4 @@
+var moduleA = require('my-module')
+var moduleB = require('your-module')
+
+moduleA.doSomething(moduleB)
